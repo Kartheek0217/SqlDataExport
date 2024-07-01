96e814808b34843eff712940f961d2ab679d880d
Karthik Jakkula <karthikj@intellectinfo.com>
2024-07-01 12:49:40 +0530

    changes made in office

--- dbManager/src/main/java/com/migration/dbManager/rest/DataMigration.java
+++ dbManager/src/main/java/com/migration/dbManager/rest/DataMigration.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.migration.dbManager.Mysql.MysqlOperations;
+import com.migration.dbManager.mysql.MysqlOperations;
 
 @RestController
 @RequestMapping("/migration")
